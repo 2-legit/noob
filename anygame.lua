735be898f3fc9fdfc19cb79aabe0a158e265f4a8
9pmX9 <109059103+Springs67@users.noreply.github.com>
2022-09-05 14:28:00 -0400

    added speed bypasses

--- anygame.lua
+++ anygame.lua
@@ -1,3 +1,90 @@
+--noboline anygame
 --script that works in every game (depends on its ac)
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Noboline 2.0(anygame script)", "Ocean")
+local Window = Library.CreateLib("Noboline 2.0", "Ocean")
+
+local Combat = Window:NewTab("Combat")
+
+local CombatSection = Combat:NewSection("General")
+
+CombatSection:NewButton("AntiAura(CANT TURN OFF!!!)", "makes it harder to hit you", function()
+    while true do
+        wait(1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 4, 0)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 4, 0)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 4, 0)
+    end
+end)
+
+local Movement = Window:NewTab("Movement")
+
+local MovementSection = Movement:NewSection("General")
+
+MovementSection:NewKeybind("Fly", "R", Enum.KeyCode.R, function()
+	game.Workspace.Gravity = 0
+    wait(2.5)
+    game.Workspace.Gravity = 196
+end)
+
+MovementSection:NewKeybind("Speed", "speeds up", Enum.KeyCode.K, function()
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
+end)
+
+MovementSection:NewButton("SpeedTPBypass", "ezezez", function()
+    while true do
+      wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * 2
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * 2
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * 2
+end
+end)
+
+MovementSection:NewKeybind("Longjump", "longer jump", Enum.KeyCode.J, function()
+	game.Players.LocalPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+    game.Workspace.Gravity = 15
+    wait(2.5)
+    game.Workspace.Gravity = 196
+end)
+
+MovementSection:NewKeybind("HighJump", "jump higher", Enum.KeyCode.H, function()
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0)
+wait(0.1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0)
+wait(0.1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0)
+wait(0.1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0)
+end)
+
+local Mechanics = Window:NewTab("Mechanics")
+
+local MechanicsSection = Mechanics:NewSection("General")
+
+MechanicsSection:NewToggle("Low Gravity", "makes gravity low", function(state)
+    if state then
+        game.Workspace.Gravity = 40
+    else
+        game.Workspace.Gravity = 196
+    end
+end)
+
+MechanicsSection:NewToggle("High Gravity", "makes gravity higher", function(state)
+    if state then
+        game.Workspace.Gravity = 500
+    else
+        game.Workspace.Gravity = 196
+    end
+end)
+
+MechanicsSection:NewKeybind("RightShift", "RightShift", Enum.KeyCode.RightShift, function()
+	Library:ToggleUI()
+end)
