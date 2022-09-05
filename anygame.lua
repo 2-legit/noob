c7bc1657ca9b59f468a6b4e605d03378ed7a109d
Vinny <103543816+VinnyPrograming@users.noreply.github.com>
2022-09-05 14:21:17 -0400

    Update anygame.lua

--- anygame.lua
+++ anygame.lua
@@ -1,2 +1,3 @@
 --script that works in every game (depends on its ac)
-
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Noboline 2.0(anygame script)", "Ocean")
