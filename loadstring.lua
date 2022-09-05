eb1cc55e2c64246ee4ed8c1ea016e1ad82d3b764
9pmX9 <109059103+Springs67@users.noreply.github.com>
2022-09-05 14:30:11 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,3 +1,4 @@
 loadstring(game:HttpGet"https://raw.githubusercontent.com/VinnyPrograming/noobbedwarscheat/main/bedwars.lua")()
 loadstring(game:HttpGet"https://raw.githubusercontent.com/VinnyPrograming/noobbedwarscheat/main/skywars.lua")()
 loadstring(game:HttpGet"https://raw.githubusercontent.com/VinnyPrograming/noobbedwarscheat/main/arsenal.lua")()
+loadstring(game:HttpGet"https://raw.githubusercontent.com/VinnyPrograming/noobbedwarscheat/main/anygame.lua")()
